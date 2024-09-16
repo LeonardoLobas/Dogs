6848d956efeb0eadcab73185703b4d1e19495f52
LeonardoLobas <leonardolobas2703@gmail.com>
2024-09-16 17:58:08 -0300

    Iniciando configs

--- Dogs-projeto/src/App.jsx
+++ Dogs-projeto/src/App.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 import "./App.css";
-import Api from "./API/Api";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import {
+    BrowserRouter,
+    createBrowserRouter,
+    Outlet,
+    Route,
+    RouterProvider,
+    Routes,
+} from "react-router-dom";
 
 const App = () => {
+    const route = createBrowserRouter([
+        {
+            path: "/",
+            element: (
+                <>
+                    <Header /> <Outlet /> <Footer />
+                </>
+            ),
+            children: [
+                { path: "/", element: <Home /> },
+                {
+                    path: "/login",
+                    element: <Login />,
+                },
+            ],
+        },
+        {
+            path: "*",
+            element: <>Ai não lobinho </>,
+        },
+    ]);
     return (
-        <>
-            <div>
-                <Api />
-            </div>
-        </>
+        <div>
+            <RouterProvider router={route} />
+        </div>
     );
 };
 
