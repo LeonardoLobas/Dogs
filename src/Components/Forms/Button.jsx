fbe763fb43afe94cebf03f62bd9d7c75761a8af9
LeonardoLobas <leonardolobas2703@gmail.com>
2024-09-30 17:45:15 -0300

    UseForm. Hook formulário.

--- src/Components/Forms/Button.jsx
+++ src/Components/Forms/Button.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-const Button = () => {
-    return <div className={styles.button}></div>;
+const Button = ({ children, props }) => {
+    return (
+        <button {...props} className={styles.button}>
+            {children}
+        </button>
+    );
 };
 
 export default Button;
