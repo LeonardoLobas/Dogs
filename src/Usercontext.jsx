582d537290344fd1d09cda236a00d6df7993206f
LeonardoLobas <leonardolobas2703@gmail.com>
2024-10-15 17:52:04 -0300

    Idealizando autologin.

--- src/Usercontext.jsx
+++ src/Usercontext.jsx
@@ -12,6 +12,25 @@ export const UserStorage = ({ children }) => {
     const [loading, setLoading] = React.useState(false);
     const [error, setError] = React.useState(null);
 
+    React.useEffect(() => {
+        async function autoLogin() {
+            const token = window.localStorage.getItem("token");
+            if (token) {
+                try {
+                    setError(null);
+                    setLoading(true);
+                    const { url, options } = TOKEN_VALIDATE_POST(token);
+                    const response = await fetch(url, options);
+                    if (!response.ok) throw new Error("Token inválido");
+                    await getUser(token);
+                } catch (erro) {
+                } finally {
+                    setLoading(false);
+                }
+            }
+        }
+    });
+
     async function getUser(token) {
         const { url, options } = USER_GET(token);
         const tokenRes = await fetch(url, options);
