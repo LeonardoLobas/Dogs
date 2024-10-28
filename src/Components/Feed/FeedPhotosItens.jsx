6ec14022787cca3a8a401b13882773492d6e7405
LeonardoLobas <leonardolobas2703@gmail.com>
2024-10-28 17:45:50 -0300

    Continuando feed.

--- src/Components/Feed/FeedPhotosItens.jsx
+++ src/Components/Feed/FeedPhotosItens.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./FeedPhotosItem.module.css";
 
-const FeedPhotosItens = () => {
-    return <div></div>;
+const FeedPhotosItens = ({ photo }) => {
+    return (
+        <li className={styles.photo}>
+            <img src={photo.src} alt={photo.title} />
+            <span>{photo.acessos}</span>
+        </li>
+    );
 };
 
 export default FeedPhotosItens;
