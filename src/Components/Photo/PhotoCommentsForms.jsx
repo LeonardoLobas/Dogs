27c4bf5efbda19e0cb7fd9d34a28c13ff1655dd2
LeonardoLobas <leonardolobas2703@gmail.com>
2024-11-11 17:54:26 -0300

    Comentarios fotos.

---
+++ src/Components/Photo/PhotoCommentsForms.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Enviar from "../../Assets/enviar.svg";
+import { COMMENT_POST } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import styles from "./PhotoCommentsForms.module.css";
+
+const PhotoCommentsForms = ({ id, setComments }) => {
+    const { request, error } = useFetch();
+    const [comment, setComment] = React.useState("");
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const { url, options } = COMMENT_POST(id, { comment });
+        const { response, json } = await request(url, options);
+        if (response.ok) {
+            setComment("");
+            setComments((comments) => [...comments, json]);
+        }
+    }
+
+    return (
+        <form className={styles.form} onSubmit={handleSubmit}>
+            <textarea
+                className={styles.textarea}
+                id="comment"
+                name="comment"
+                placeholder="Comente"
+                value={comment}
+                onChange={({ target }) => setComment(target.value)}
+            />
+            <button className={styles.button}>
+                <img src={Enviar} alt="enviar" className={styles.svg} />
+            </button>
+            <Error error={error} />
+        </form>
+    );
+};
+
+export default PhotoCommentsForms;
